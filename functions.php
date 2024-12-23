05bbe56b741479d914518ac89189d9043af5de5c
Noah Miller <bruno.tribst@gmail.com>
2024-12-23 11:34:12 -0300

    upload1

---
+++ functions.php
@@ -0,0 +1,6 @@
+﻿<?php
+function blank_woocommerce_theme_setup() {
+    // Add WooCommerce support
+    add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'blank_woocommerce_theme_setup');
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+get_header();
+
+	echo '<h1>Blank WooCommerce Theme</h1>';
+
+get_footer();
